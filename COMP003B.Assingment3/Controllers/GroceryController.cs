1a1eafe4b26c6bd33f587b6f459545b6b7ce5104
Logan Jones <lpj11101@gmail.com>
2024-04-10 08:43:14 -0700

    Added GroceryModel

---
+++ COMP003B.Assingment3/Controllers/GroceryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assingment3.Controllers
+{
+    public class GroceryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assingment3/Models/GroceryModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assingment3.Models
+{
+    public class GroceryModel
+    {
+        [Required]
+        public int PLU {  get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name="Product Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0,500)]
+        public string Quantity {  get; set; }
+
+
+
+    }
+}
